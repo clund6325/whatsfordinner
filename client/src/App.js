0b4099ef1d6c94aeba8ac2bb6cbc2c6b66ad169c
Cameron Lund <clund6314@gmail.com>
2021-08-14 14:17:18 -0600

    begin coding App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Home from './components/shared/Home';
+import Nomatch from './components/shared/Nomatch';
+import MainNavbar from './components/shared/MainNavbar';
+import About from './components/shared/About';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import FetchUser from './components/auth/FetchUser';
+import ProtectedRoute from './components/auth/ProtectedRoute'
+import Plans from './components/plans/Plans';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <MainNavbar />
+    <FetchUser>
+      <Container>
+        <Switch>
+          <ProtectedRoute exact path='/home' component={Home} />
+          <ProtectedRoute exact path='/plans' component={Plans}/>
+        </Switch>
+      </Container>
+    </FetchUser>
+  </>
+)
 
 export default App;
